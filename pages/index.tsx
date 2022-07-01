93cbdfb72691a4f08e6db8f0b82d69171e098790
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-07-01 18:06:46 +0200

    scafholding

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
-  return (
-   <h2 className='text-5xl'>helloworkd</h2>
-  )
+  return <Header/>
+  
+  
 }
 
 export default Home
